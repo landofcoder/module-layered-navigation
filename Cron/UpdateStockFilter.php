3f265ff0cd8512e67cd27cbca0146e657c962c24
landofcoder <landofcoder@gmail.com>
2021-09-14 03:21:24 +0700

    up

--- Cron/UpdateStockFilter.php
+++ Cron/UpdateStockFilter.php
@@ -38,64 +38,7 @@ class UpdateStockFilter
             JOIN eav_attribute ap ON ap.attribute_id = t.attribute_id
             SET value = stock_status WHERE attribute_code = '".InstallData::ATTRIBUTE_CODE."'";
             $connection->query($query);
-
-            $instockProductIds = $this->getProductsWithStockFilter(\Magento\CatalogInventory\Model\Stock::STOCK_IN_STOCK);
-            $outstockProductIds = $this->getProductsWithStockFilter(\Magento\CatalogInventory\Model\Stock::STOCK_OUT_OF_STOCK);
-            // Update query
-            if ($outstockProductIds) {
-                $outstockQuery = "UPDATE " . $table . " t
-                JOIN eav_attribute ap ON ap.attribute_id = t.attribute_id
-                SET value = ".\Lof\LayeredNavigation\Model\Config\Source\Options::OUTSTOCK_ID." WHERE attribute_code = '".InstallData::ATTRIBUTE_CODE."' and t.entity_id IN (?)";
-                $connection->query($outstockQuery, implode(",", $outstockProductIds));
-            }
-            if ($instockProductIds) {
-                $instockQuery = "UPDATE " . $table . " t
-                JOIN eav_attribute ap ON ap.attribute_id = t.attribute_id
-                SET value = ".\Lof\LayeredNavigation\Model\Config\Source\Options::INSTOCK_ID." WHERE attribute_code = '".InstallData::ATTRIBUTE_CODE."' and t.entity_id IN (?)";
-                $connection->query($instockQuery, implode(",", $instockProductIds));
-            }
-            
         }
         return $this;
-    }
-
-    /**
-     * @param $value
-     * @return string
-     */
-
-    public function getProductsWithStockFilter($value)
-    {
-        $connection =  $this->resourceConnection->getConnection();
-        $tableName = $this->resourceConnection->getTableName('catalog_product_entity');
-        $select = $connection->select()->from(
-            ['e' => $tableName]
-            )->where('1 = ?', 1);
-        
-        $joinCondition = $this->stockResource->getConnection()->quoteInto(
-            'e.entity_id = stock_status_idx.product_id AND stock_status_idx.stock_id = ?',
-            \Magento\CatalogInventory\Model\Stock::DEFAULT_STOCK_ID
-        );
-        $select->join(
-            [
-                'stock_status_idx' => $this->stockResource->getMainTable()
-            ],
-            $joinCondition,
-            []
-        );
-
-        // reset columns, order and limitation conditions
-        $select->reset(\Magento\Framework\DB\Select::COLUMNS);
-        $select->reset(\Magento\Framework\DB\Select::ORDER);
-        $select->reset(\Magento\Framework\DB\Select::LIMIT_COUNT);
-        $select->reset(\Magento\Framework\DB\Select::LIMIT_OFFSET);
-        $select->where('stock_status_idx.stock_status = ?', $value);
-        $select->columns(
-            [
-                'entity_id'
-            ]
-        );
-
-        return $connection->fetchCol($select);
     }
 }
